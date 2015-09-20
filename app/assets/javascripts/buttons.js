2406c0594cc68c8df82d385aa910a8b7a5412953
Donald Wade <donald@rwck.com.au>
2015-09-21 09:32:12 +1000

    further adjustments

--- app/assets/javascripts/buttons.js
+++ app/assets/javascripts/buttons.js
@@ -1,11 +1,24 @@
+
+/*
 function mouseDown() {
+  var className = (this.attr('class'));
+  className({
+    'box-shadow': '0 0 0 0',
+  });
+};
+*/
+
+
+function pressDown() {
   $(".experimental").css({
     'box-shadow': '0 0 0 0',
   });
 };
 
-function mouseUp() {
+
+function pressUp() {
   $(".experimental").css({
-    'box-shadow': '1px 2px 4px 0px rgba(0,0,0,0.50)', 
+    'box-shadow': '1px 2px 4px 0px rgba(0,0,0,0.50)',
   });
+  // alert("poop");
 };
--- app/assets/javascripts/color.js
+++ app/assets/javascripts/color.js
@@ -57,8 +57,6 @@ function changeColor() {
 
 };
 
-
-
 $(function() {
   changeColor();
 });
