beaddb46639ecfd3dcb8dc72d819203beee68e0d
Donald Wade <donald@rwck.com.au>
2015-09-20 21:10:43 +1000

    further adjustments

--- app/assets/javascripts/buttons.js
+++ app/assets/javascripts/buttons.js
@@ -1,11 +1,11 @@
 function mouseDown() {
   $(".experimental").css({
-    'background-color': hsla2,
     'box-shadow': '0 0 0 0',
+  });
 };
 
 function mouseUp() {
   $(".experimental").css({
-    'background-color': hsla1,
     'box-shadow': '1px 2px 4px 0px rgba(0,0,0,0.50)',
+  });
 };
--- app/assets/javascripts/color.js
+++ app/assets/javascripts/color.js
@@ -57,18 +57,6 @@ function changeColor() {
 
 };
 
-function mouseDown() {
-  $(".experimental").css({
-    'box-shadow': '0 0 0 0',
-  });
-};
-
-function mouseUp() {
-  $(".experimental").css({
-    'box-shadow': '1px 2px 4px 0px rgba(0,0,0,0.50)',
-  });
-};
-
 
 
 $(function() {
