8acb8d025159b2fe750c9c090796f32a6f158a70
Donald Wade <donald@rwck.com.au>
2015-09-21 22:09:00 +1000

    added some javascript colour dooberies

--- app/assets/javascripts/buttons.js
+++ app/assets/javascripts/buttons.js
@@ -1,22 +1,25 @@
-
-/*
-function mouseDown() {
-  var className = (this.attr('class'));
-  className({
-    'box-shadow': '0 0 0 0',
-  });
-};
-*/
-
-function pressDown() {
-  $(".experimental").css({
+function pressDown(name) {
+  myClass = name.className;
+  $("."+myClass).css({
     'box-shadow': '0.5px 1px 2px 0 rgba(0,0,0,0.5)',
   });
+
+  for (i=0; i<5; i++) {
+    $("."+myClass+i).css({
+      'box-shadow': '0.5px 1px 2px 0 rgba(0,0,0,0.5)',
+    });
+  };
 };
 
-function pressUp() {
-  $(".experimental").css({
+function pressUp(name) {
+  myClass = name.className;
+  $("."+myClass).css({
     'box-shadow': '1px 2px 4px 0px rgba(0,0,0,0.50)',
   });
-  // alert("poop");
+  // // alert(myClass.split(""))
+  for (i=0; i<5; i++) {
+    $("."+myClass+i).css({
+      'box-shadow': '1px 2px 4px 0px rgba(0,0,0,0.50)',
+    });
+  };
 };
--- app/assets/javascripts/color.js
+++ app/assets/javascripts/color.js
@@ -1,34 +1,41 @@
 function changeColor() {
   var hue = Math.floor(Math.random() * 360);
 
-  if (hue >= 180) {
-    var hue2 = hue - 180;
-  }
-  else if (hue <180) {
-    var hue2 = hue + 180;
-  }
+  var hue2 = hue + 180
+  var hue3 = hue + 135
+  var hue4 = hue + 225
 
-  if (hue >= 225) {
-    var hue3 = hue - 225;
-  }
-  else if (hue <225) {
-    var hue3 = hue + 225;
-  }
-  if (hue >= 135) {
-    var hue4 = hue - 135;
-  }
-  else if (hue <135) {
-    var hue4 = hue + 135;
-  }
+  // if (hue >= 180) {
+  //   var hue2 = hue - 180;
+  // }
+  // else if (hue <180) {
+  //   var hue2 = hue + 180;
+  // }
+  //
+  // if (hue >= 225) {
+  //   var hue3 = hue - 225; // this maths could be wrong
+  // }
+  // else if (hue <225) {
+  //   var hue3 = hue + 135; // this maths could be wrong
+  // }
+  // if (hue >= 135) {
+  //   var hue4 = hue - 135; // this maths could be wrong
+  // }
+  // else if (hue <135) {
+  //   var hue4 = hue + 225; // this maths could be wrong
+  // }
 
-  var saturation = 100;
-  var lightness = 25;
+  var saturation = 50;
+  var lightness = 50;
+  var opacity = 1;
   var textLightness = lightness + 100;
-  var opacity = 0.4;
+  var textOpacity = opacity * 2.5
+
 
   var hsla1 = "hsla("+hue+ ", " +saturation+ "%, " +lightness+ "%, " +opacity+ ")";
   var hsla2 = "hsla("+hue2+ ", " +saturation+ "%, " +lightness+ "%, " +opacity+ ")";
-  var hsla3 = "hsla("+hue3+ ", " +saturation+ "%, " +textLightness+ "%, " +opacity+ ")";
+  var hsla3 = "hsla("+hue3+ ", " +saturation+ "%, " +lightness+ "%, " +opacity+ ")";
+  var hsla301 = "hsla("+hue3+ ", " +saturation+ "%, " +textLightness+ "%, " +textOpacity+ ")";
   var hsla4 = "hsla("+hue4+ ", " +saturation+ "%, " +lightness+ "%, " +opacity+ ")";
 
 
@@ -41,19 +48,36 @@ function changeColor() {
     // $(".experimental").css("background-color", hsla1);
     $(".experimental").css({
       'background-color': hsla1,
-      //'box-shadow': "0 0 0 0",
-
+      'color': hsla301,
     });
+
+    $(".experimental2").css({
+        'background-color': hsla2,
+        'color': hsla301,
+    });
+
+    $(".experimental3").css({
+        'background-color': hsla3,
+        'color': hsla301,
+    });
+
+    $(".experimental4").css({
+      'background-color': hsla4,
+      'color': hsla301,
+    });
+      //'box-shadow': "0 0 0 0",
 
     // }"background-color", hsla1);
     //   // "box-shadow": 0 0 0 0;
     // $(".experimental").css("border", "solid 2px "+hsla2);
-    //$(".experimental").css("color", hsla3); // "solid 14px "+hsla2);
+    $(".experimental").css("color", hsla301); // "solid 14px "+hsla2);
     // $(".experimental").css("color", hsla3); // "solid 14px "+hsla2);
     // $(".experimental").css("box-shadow", 0px 0px 0px 0px); // rgba(0,0,0,0.50);
 
     // $(".experimental").css("border", "solid 14px "+hsla2);
-    // alert(hsla1, hsla2, hsla3, hsla4);
+
+    // This gives a useful alert as to what colours are being pumped out.
+    //alert([hsla1, hsla2, hsla3, hsla4]);
 
 };
 
